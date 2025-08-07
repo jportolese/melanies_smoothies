c9f76bebb6faa7d2b4afb497df83268be9f6c03e
Jamie Portolese <j.portolese@comcast.net>
2025-08-07 16:20:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-   ingredients_string = ''
+  ingredients_string = ''
 
    for fruit_chosen in ingredients_list:
      ingredients_string += fruit_chosen + ' '
@@ -44,7 +44,7 @@ if ingredients_list:
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
      
      time_to_insert = st.button('Submit Order')
-       if time_to_insert:
-         session.sql(my_insert_stmt).collect()
+     if time_to_insert:
+       session.sql(my_insert_stmt).collect()
        
        st.success('Your Smoothie is ordered, ' + name_on_order +'!', icon="✅")
